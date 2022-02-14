175b20b0541cd5eb1a8c0c6e67b46fb83d9dc672
ha^3 <>
2022-02-14 21:48:46 +0800

    init

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,77 +1,92 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  TheRelationDAO,
-  Approval,
-  ApprovalForAll,
   FollowEvt,
   ProfileEvt,
   RegisterEvt,
   Transfer,
-  UnFollowEvt
-} from "../generated/TheRelationDAO/TheRelationDAO"
-import { ExampleEntity } from "../generated/schema"
+  UnFollowEvt,
+} from "../generated/TheRelationDAO/TheRelationDAO";
+import { People, Follow } from "../generated/schema";
+import { BigInt, store } from "@graphprotocol/graph-ts";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleRegisterEvt(event: RegisterEvt): void {
+  let id = event.params._from.toString();
+  let people = People.load(id);
+  if (people == null) {
+    people = new People(id);
+    people.owner = event.params._owner;
+    people.followCount = BigInt.fromI32(0);
+    people.followedCount = BigInt.fromI32(0);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  people.profile = event.params._profile;
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.is_follow(...)
-  // - contract.name(...)
-  // - contract.next_user(...)
-  // - contract.ownerOf(...)
-  // - contract.query_follow_sum(...)
-  // - contract.query_followed_sum(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
+  people.save();
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+export function handleTransfer(event: Transfer): void {
+  let id = event.params.tokenId.toString();
+  let people = People.load(id);
+  if (people == null) {
+    people = new People(id);
+    people.followCount = BigInt.fromI32(0);
+    people.followedCount = BigInt.fromI32(0);
+    people.profile = "";
+  }
 
-export function handleFollowEvt(event: FollowEvt): void {}
+  people.owner = event.params.to;
 
-export function handleProfileEvt(event: ProfileEvt): void {}
+  people.save();
+}
 
-export function handleRegisterEvt(event: RegisterEvt): void {}
+export function handleFollowEvt(event: FollowEvt): void {
+  let id = event.params._from.toString() + ":" + event.params._to.toString();
+  let follow = Follow.load(id);
 
-export function handleTransfer(event: Transfer): void {}
+  if (follow == null) {
+    follow = new Follow(id);
+    follow.fromId = event.params._from;
+    follow.toId = event.params._to;
+    follow.save();
 
-export function handleUnFollowEvt(event: UnFollowEvt): void {}
+    let followPeople = People.load(event.params._from.toString());
+    if (followPeople) {
+      followPeople.followCount?.plus(BigInt.fromI32(1));
+      followPeople.save();
+    }
+
+    let followedPeople = People.load(event.params._to.toString());
+    if (followedPeople) {
+      followedPeople.followedCount?.plus(BigInt.fromI32(1));
+      followedPeople.save();
+    }
+  }
+}
+
+export function handleProfileEvt(event: ProfileEvt): void {
+  let id = event.params._from.toString();
+  let people = People.load(id);
+  if (people != null) {
+    people.profile = event.params._profile;
+    people.save();
+  }
+}
+
+export function handleUnFollowEvt(event: UnFollowEvt): void {
+  let id = event.params._from.toString() + ":" + event.params._to.toString();
+  let follow = Follow.load(id);
+  if (follow != null) {
+    store.remove("Follow", id);
+
+    let followPeople = People.load(event.params._from.toString());
+    if (followPeople) {
+      followPeople.followCount?.minus(BigInt.fromI32(1));
+      followPeople.save();
+    }
+
+    let followedPeople = People.load(event.params._to.toString());
+    if (followedPeople) {
+      followedPeople.followedCount?.minus(BigInt.fromI32(1));
+      followedPeople.save();
+    }
+  }
+}
